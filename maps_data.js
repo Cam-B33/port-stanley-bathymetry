9d72cd5dd2ea0e4e54a42ef8f35d61eeec5f1805
Cameron <cameron@example.com>
2026-01-12 12:40:01 -0500

    Add first bathymetry map

--- maps_data.js
+++ maps_data.js
@@ -1,4 +1,16 @@
 const mapsData = {
-  "maps": [],
-  "latest": null
+  "maps": [
+    {
+      "filename": "bathymetry_2024_09_30.png",
+      "date": "2024-09-30",
+      "description": "Initial bathymetry mapping",
+      "added": "2026-01-12T12:40:01.605208"
+    }
+  ],
+  "latest": {
+    "filename": "bathymetry_2024_09_30.png",
+    "date": "2024-09-30",
+    "description": "Initial bathymetry mapping",
+    "added": "2026-01-12T12:40:01.605208"
+  }
 };
